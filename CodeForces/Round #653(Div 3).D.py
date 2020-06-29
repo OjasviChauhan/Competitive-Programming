43206af434d2bebc46542cb9b0f230f98a9d0eb2
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-06-29 17:59:02 +0530

    Update Round #653(Div 3).D.py

--- CodeForces/Round #653(Div 3).D.py
+++ CodeForces/Round #653(Div 3).D.py
@@ -1,23 +1,57 @@
-def solve(n,k,A):
-	x = 0
-	count = 0
-	while(len(A)>0):
-		for i in range(0, len(A)):
-			if(A[i]%k == 0):
-				del A[i]
-				x -= 1
-				count -= 1
-				break
-			elif (A[i]+x)%k == 0:
-				# A[i] += x
-				del A[i]
-				break
-		x += 1
-		count += 1
-	return count
+#include <bits/stdc++.h>
+#include <string>
+#include <cmath>
+#include <sstream>
+#define FOR(i, a, b) for (long long int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(long long int i = (a); i >=(b); i--)
+#define iterate(i, A) for(auto i=A.begin(); i != A.end(); ++i)
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+typedef long long int ll;
+using namespace std;
+ll mod1=1000000007;
+ll mod2=67280421310721;
+ll mod3=998244353;
+ll INF=1e18;
+ 
+int solve(int n,int k,vector<int> A){
+    int x=0, count=0;
+    while(A.size() > 0){
+        iterate(i, A){
+            if(*i % k == 0){
+                A.erase(i);
+                x--;
+                count--;
+                break;
+            }
+            else if((*i+x)%k == 0){
+                A.erase(i);
+                break;
+            }
+        }
+        x++;
+        count++;
+    }
+    return count;
+}
 
-t = int(input())
-for _ in range(t):
-	n,k = map(int, input().split())
-	A = list(map(int, input().split()))
-	print(solve(n,k,A))
+int main(){
+ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       int n,k,input;
+       cin >> n >> k;
+       vector<int> A;
+       FOR(i,0,n){
+           cin >> input;
+           A.pb(input);
+       }
+       int z = solve(n,k,A);
+       cout << z << "\n";
+    }
+  return 0;
+}
