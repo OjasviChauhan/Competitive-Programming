e1eb3c3a092d1621727e100f675473436d8b5850
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-08-31 22:14:42 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ InterviewBit/Dynamic Programming/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,26 @@
+int Solution::solve(string y) {
+    string x = y;
+    reverse(y.begin(), y.end());
+    int m = x.size();
+    int n = m;
+    
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0 || j==0){
+                dp[i][j] = 0;
+            }
+        }
+    }
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){        
+            if(x[i-1] == y[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+    }
+    return dp[m][n];
+}
