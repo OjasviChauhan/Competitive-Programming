86ed283a072f1a8e406c1d565958626f9625da7f
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-12-07 15:12:29 +0530

    Create 1679. Max Number of K-Sum Pairs.cpp

---
+++ LeetCode Problems/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> Map;
+        for (auto &num: nums) Map[num]++;  // count freq of nums
+        int ans = 0;
+  
+        for(auto it=Map.begin(); it!=Map.end(); ++it){
+            int num = it->first, count = it->second;
+            // if num is half of k add half of it's count in ans
+            if(k - num == num) ans += count/2;   
+            else if(Map.count(k - num)){   
+                // find k-num in nums and add min freq of num or k-num to ans
+                int Min = min(count, Map[k-num]);
+                ans += Min;
+                Map[num] -= Min;
+                Map[k-num] -= Min;
+            }
+        }
+        return ans;
+    }
+};
